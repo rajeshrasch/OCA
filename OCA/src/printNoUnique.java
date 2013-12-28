44080dbd1ea551cdc877717f7ff2e8a9b1bfe197
rajeshrasch <emailforrajesh@gmail.com>
2013-12-28 12:52:34 -0800

    Added Program

---
+++ OCA/src/printNoUnique.java
@@ -0,0 +1,27 @@
+
+public class printNoUnique {
+
+	public static void main(String[] args) {
+		int i=1;
+		
+		while(i<=100)
+		{
+			
+		if(i%3==0&&i%5==0)
+			System.out.println("buzzfuzz");
+		else if(i%3==0)
+			System.out.println("Buzz");
+		else if(i%5==0)
+			System.out.println("fuzz");
+		else
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		}
+
+
+	}
+
+}
